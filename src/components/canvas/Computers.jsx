ce2ceb23fce32ae76b9c3e2546a6f44db2e5fa60
Anthony Tamzil <anthony.tamzil@gmail.com>
2023-07-27 22:27:26 +0800

    Update computer for mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
-import {Suspense, useEffect, useState} from 'react';
-import {Canvas} from "@react-three/fiber";
-import {OrbitControls, Preload, useGLTF} from "@react-three/drei";
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
@@ -12,7 +13,7 @@ const Computers = ({ isMobile }) => {
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
       <spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024}/>
-      <primitive object={computer.scene} scale={isMobile ? 0.5 : 0.75} position={isMobile ? [0, -1.8, -1.6] : [0, -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]}/>
+      <primitive object={computer.scene} scale={isMobile ? 0.5 : 0.75} position={isMobile ? [0, -1.8, -1.0] : [0, -3.25, -1.5]} rotation={isMobile ? [-0.01, 0.5, 0] : [-0.01, -0.2, -0.1]}/>
     </mesh>
   )
 }
@@ -40,13 +41,13 @@ const ComputersCanvas = () => {
     <Canvas frameloop="demand" shadows camera={{position: [20, 3, 5], fov: 25}} gl={{preserveDrawingBuffer: true}}>
       <Suspense fallback={<CanvasLoader />}>
         {isMobile ? "" : (
-        <OrbitControls 
-          enableZoom={false} 
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-          autoRotate
-          autoRotateSpeed={0.6}
-        />
+          <OrbitControls 
+            enableZoom={false} 
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+            autoRotate
+            autoRotateSpeed={0.6}
+          />
         )}
         <Computers isMobile={isMobile}/>
       </Suspense>
